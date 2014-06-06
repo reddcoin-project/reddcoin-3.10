940e7aa5d29c49f76dfd4bbcb7e0581515ef7b0f
Cory Fields <cory-nospam-@coryfields.com>
2014-06-06 10:23:34 -0400

    secp256k1: add libtool as a dependency
    (cherry picked from commit b150b09edcd3b6b7ca5f26ca6ebee92bbad97089)
    
    # Conflicts:
    #	autogen.sh
    #	doc/build-osx.md

--- autogen.sh
+++ autogen.sh
@@ -2,10 +2,7 @@
 set -e
 srcdir="$(dirname $0)"
 cd "$srcdir"
-
 if [ -z ${LIBTOOLIZE} ] && GLIBTOOLIZE="`which glibtoolize 2>/dev/null`"; then
-  LIBTOOLIZE="${GLIBTOOLIZE}"
-  export LIBTOOLIZE
+  export LIBTOOLIZE="${GLIBTOOLIZE}"
 fi
-
 autoreconf --install --force
