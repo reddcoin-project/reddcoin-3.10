e4d473df3dd6347a34bcaf13e34ffd49ffd24259
jtimon <jtimon@monetize.io>
2014-06-20 20:55:42 +0200

    replace ComputeMinWork with CheckMinWork
    (cherry picked from commit 654871d43677947d124673c9e0dd2984f0d3ca61)
    
    # Conflicts:
    #	src/main.cpp
    #	src/pow.cpp
    #	src/pow.h

--- src/main.cpp
+++ src/main.cpp
@@ -2753,12 +2753,7 @@ bool AcceptBlockHeader(CBlockHeader& block, CValidationState& state, CBlockIndex
             return state.DoS(100, error("CheckBlockHeader() : block with timestamp before last checkpoint"),
                              REJECT_CHECKPOINT, "time-too-old");
         }
-        bool fOverflow = false;
-		uint256 bnNewBlock;
-        bnNewBlock.SetCompact(block.nBits, NULL, &fOverflow);
-        uint256 bnRequired;
-        bnRequired.SetCompact(ComputeMinWork(pcheckpoint->nBits, deltaTime));
-        if (bnNewBlock > bnRequired)
+        if (!CheckMinWork(block.nBits, pcheckpoint->nBits, deltaTime))
         {
             return state.DoS(100, error("CheckBlockHeader() : block with too little proof-of-work"),
                              REJECT_INVALID, "bad-diffbits");
