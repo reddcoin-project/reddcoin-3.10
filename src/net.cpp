9177ebeee7e6b259199f6bf42b33c74ef1560017
Philip Kaufmann <phil.kaufmann@t-online.de>
2014-06-22 14:51:38 +0200

    remove unused CNode::Cleanup()
    (cherry picked from commit 2831a03b798e0eea724250a6ba15cb637800354d)

--- src/net.cpp
+++ src/net.cpp
@@ -509,10 +509,6 @@ void CNode::CloseSocketDisconnect()
         pnodeSync = NULL;
 }
 
-void CNode::Cleanup()
-{
-}
-
 void CNode::PushVersion()
 {
     int nBestHeight = g_signals.GetHeight().get_value_or(0);
@@ -758,7 +754,6 @@ void ThreadSocketHandler()
 
                     // close socket and cleanup
                     pnode->CloseSocketDisconnect();
-                    pnode->Cleanup();
 
                     // hold in disconnected pool until all refs are released
                     if (pnode->fNetworkNode || pnode->fInbound)
