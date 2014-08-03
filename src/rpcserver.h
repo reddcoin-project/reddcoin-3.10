ee22815ceb2a5d156cb899b235c0eacfec4e50a5
Daniel Kraft <d@domob.eu>
2014-08-03 18:12:19 +0200

    Implement "getchaintips" RPC command to monitor blockchain forks.
    Port over https://github.com/chronokings/huntercoin/pull/19 from
    Huntercoin:  This implements a new RPC command "getchaintips" that can be
    used to find all currently active chain heads.  This is similar to the
    -printblocktree startup option, but it can be used without restarting
    just via the RPC interface on a running daemon.
    
    (cherry picked from commit b33bd7a3be1cbcc8d255178307976b7762125b18)

--- src/rpcserver.h
+++ src/rpcserver.h
@@ -207,6 +207,7 @@ extern json_spirit::Value getblock(const json_spirit::Array& params, bool fHelp)
 extern json_spirit::Value gettxoutsetinfo(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value gettxout(const json_spirit::Array& params, bool fHelp);
 extern json_spirit::Value verifychain(const json_spirit::Array& params, bool fHelp);
+extern json_spirit::Value getchaintips(const json_spirit::Array& params, bool fHelp);
 
 // PoSV
 extern json_spirit::Value getstakinginfo(const json_spirit::Array& params, bool fHelp);
