18af434148edbeb2d48f32a8bd8f7f43fc2f95d7
Pieter Wuille <pieter.wuille@gmail.com>
2014-10-08 18:48:59 -0700

    Add SCRIPT_VERIFY_MINIMALDATA (BIP62 rules 3 and 4)
    Also use the new flag as a standard rule, and replace the IsCanonicalPush
    standardness check with it (as it is more complete).
    
    (cherry picked from commit 698c6abb25c1fbbc7fa4ba46b60e9f17d97332ef)
    
    # Conflicts:
    #	src/script/standard.h

--- src/script/standard.h
+++ src/script/standard.h
@@ -40,8 +40,9 @@ static const unsigned int MANDATORY_SCRIPT_VERIFY_FLAGS = SCRIPT_VERIFY_P2SH;
 // with. However scripts violating these flags may still be present in valid
 // blocks and we must accept those blocks.
 static const unsigned int STANDARD_SCRIPT_VERIFY_FLAGS = MANDATORY_SCRIPT_VERIFY_FLAGS |
+                                                         SCRIPT_VERIFY_DERSIG |
                                                          SCRIPT_VERIFY_STRICTENC |
-														 SCRIPT_VERIFY_DERSIG |
+                                                         SCRIPT_VERIFY_MINIMALDATA |
                                                          SCRIPT_VERIFY_NULLDUMMY;
 
 // For convenience, standard but not mandatory verify flags.
