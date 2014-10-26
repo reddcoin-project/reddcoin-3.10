78bc23d23fd71a447380a9f82b26cbdc27161796
Michael Ford <fanquake@gmail.com>
2014-10-26 14:32:29 +0800

    Update comments in version to be doxygen compatible
    (cherry picked from commit 6395ba30366949b8631a7bf56c1898f1667284c8)
    
    # Conflicts:
    #	src/clientversion.cpp
    #	src/version.h

--- src/version.h
+++ src/version.h
@@ -1,34 +1,34 @@
-// Copyright (c) 2012 The Bitcoin developers
-// Distributed under the MIT/X11 software license, see the accompanying
+// Copyright (c) 2012-2014 The Bitcoin developers
+// Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
 
 #ifndef BITCOIN_VERSION_H
 #define BITCOIN_VERSION_H
 
-//
-// network protocol versioning
-//
+/**
+ * network protocol versioning
+ */
 
 static const int PROTOCOL_VERSION = 80000;
 
-// initial proto version, to be increased after version/verack negotiation
+//! initial proto version, to be increased after version/verack negotiation
 static const int INIT_PROTO_VERSION = 209;
 
-// disconnect from peers older than this proto version
+//! disconnect from peers older than this proto version
 static const int MIN_PEER_PROTO_VERSION = 80000;
 
-// nTime field added to CAddress, starting with this version;
-// if possible, avoid requesting addresses nodes older than this
+//! nTime field added to CAddress, starting with this version;
+//! if possible, avoid requesting addresses nodes older than this
 static const int CADDR_TIME_VERSION = 31402;
 
-// only request blocks from nodes outside this range of versions
+//! only request blocks from nodes outside this range of versions
 static const int NOBLKS_VERSION_START = 32000;
 static const int NOBLKS_VERSION_END = 32400;
 
-// BIP 0031, pong message, is enabled for all versions AFTER this one
+//! BIP 0031, pong message, is enabled for all versions AFTER this one
 static const int BIP0031_VERSION = 60000;
 
-// "mempool" command, enhanced "getdata" behavior starts with this version
+//! "mempool" command, enhanced "getdata" behavior starts with this version
 static const int MEMPOOL_GD_VERSION = 60002;
 
 #endif // BITCOIN_VERSION_H
