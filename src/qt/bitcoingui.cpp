cb86131ef8cb6740c8acafa82dd33002cca8cdb6
Jonas Schnelli <jonas.schnelli@include7.ch>
2014-11-17 15:51:33 +0100

    [Qt, OSX] QProgressBar CPU-Issue workaround
    fixes #5295
    
    (cherry picked from commit 6093aa1bb03b03331cc2f48d8f6b749f8817c016)

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -57,6 +57,20 @@
 #include <QUrlQuery>
 #endif
 
+#ifdef Q_OS_MAC
+#include <QEvent>
+
+// workaround for Qt OSX Bug:
+// https://bugreports.qt-project.org/browse/QTBUG-15631
+// QProgressBar uses around 10% CPU even when app is in background
+class QProgressBarMac : public QProgressBar
+{
+    bool event(QEvent *e) {
+        return (e->type() != QEvent::StyleAnimationUpdate) ? QProgressBar::event(e) : false;
+    }
+};
+#endif
+
 const QString BitcoinGUI::DEFAULT_WALLET = "~Default";
 extern int64_t nLastCoinStakeSearchInterval;
 double GetPoSVKernelPS();
@@ -188,7 +202,11 @@ BitcoinGUI::BitcoinGUI(bool fIsTestnet, QWidget *parent) :
     // Progress bar and label for blocks download
     progressBarLabel = new QLabel();
     progressBarLabel->setVisible(false);
+#ifdef Q_OS_MAC
+    progressBar = new QProgressBarMac();
+#else
     progressBar = new QProgressBar();
+#endif
     progressBar->setAlignment(Qt::AlignCenter);
     progressBar->setVisible(false);
 
