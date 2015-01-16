60d7d8ad7ac3d56e36a47fe6514683e37d98eda3
Cory Fields <cory-nospam-@coryfields.com>
2015-01-16 16:27:12 -0500

    qt: fix broken unicode chars on osx 10.10
    - qt: avoid hard-coding font names
    
    They may not contain all necessary characters for a language
    
    - qt: fix broken unicode chars on osx 10.10
    
    The default font changed again.
    
    The real fix is to compile qt against a >= 10.8 sdk, but this is simple enough
    to backport to 0.10 to avoid having to do that there.
    
    Note: NSAppKitVersionNumber is a double and there's no official value for
    NSAppKitVersionNumber10_10. Since == isn't reliable for doubles, use Apple's
    guidelines for testing versions here:
    https://developer.apple.com/library/mac/releasenotes/AppKit/RN-AppKit/
    
    Chinese and Japanese fonts have been hard-coded as well, otherwise they fail to
    show up at all.
    
    - qt: fonts: allow SubstituteFonts to filter based on user's language
    
    SubstituteFonts() has been moved to after app identification so that QSettings
    are accessible.
    
    Github-Pull: #5671
    Rebased-From: 73cd4edb4f1ff98c20549a609e96fa40834f5e73 52954e6efd373c14736237c4c79769bf00f5dfb8 f5ad78b34af8f50e12fab63b331768b96ec2c779
    (cherry picked from commit 58259ad1edf14a4f514fe5f837ac7d1d99b58422)

--- src/qt/splashscreen.cpp
+++ src/qt/splashscreen.cpp
@@ -40,7 +40,7 @@ SplashScreen::SplashScreen(Qt::WindowFlags f, const NetworkStyle *networkStyle) 
     QString titleText_versionText = titleText + QString(" ") + versionText;
     QString titleAddText    = networkStyle->getTitleAddText();
 
-    QString font            = "Arial";
+    QString font            = QApplication::font().toString();
 
     // load the bitmap for writing some text over it
     pixmap     = networkStyle->getSplashImage();
