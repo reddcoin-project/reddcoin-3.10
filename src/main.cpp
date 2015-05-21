4f33c94dd8ac88106c72b136825e4164b17da7a9
Suhas Daftuar <sdaftuar@gmail.com>
2015-05-21 13:29:09 -0400

    Ignore getheaders requests when not synced.
    Rebased-From: a1ba0778dd3c784046dea334e5d39f37eca264f7
    Github-Pull: #6172
    (cherry picked from commit b4bbad18ef0d3b1643d2c78c911e854b777330b8)

--- src/main.cpp
+++ src/main.cpp
@@ -4267,6 +4267,9 @@ bool static ProcessMessage(CNode* pfrom, string strCommand, CDataStream& vRecv, 
 
         LOCK(cs_main);
 
+        if (IsInitialBlockDownload())
+            return true;
+
         CBlockIndex* pindex = NULL;
         if (locator.IsNull())
         {
