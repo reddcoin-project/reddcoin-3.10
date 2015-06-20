cad6cf135d4f3909b55fcecadb5bb8b764440412
Jonas Schnelli <jonas.schnelli@include7.ch>
2015-06-20 20:27:28 +0200

    [Qt] add ui signal for banlist changes
    (cherry picked from commit 5f421329508d6e86c410282c7de39d7e80b0b289)
    
    # Conflicts:
    #	src/rpcnet.cpp

--- src/rpcnet.cpp
+++ src/rpcnet.cpp
@@ -11,6 +11,7 @@
 #include "protocol.h"
 #include "sync.h"
 #include "timedata.h"
+#include "ui_interface.h"
 #include "util.h"
 #include "version.h"
 
@@ -509,6 +510,8 @@ Value setban(const Array& params, bool fHelp)
     }
 
     DumpBanlist(); //store banlist to disk
+    uiInterface.BannedListChanged();
+
     return Value::null;
 }
 
@@ -555,6 +558,8 @@ Value clearbanned(const Array& params, bool fHelp)
 
     CNode::ClearBanned();
     DumpBanlist(); //store banlist to disk
+    
+    uiInterface.BannedListChanged();
 
     return Value::null;
 }
