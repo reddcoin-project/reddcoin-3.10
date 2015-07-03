09d3c854b234c42e66bc610418d7ae6b6e86fc21
Philip Kaufmann <phil.kaufmann@t-online.de>
2015-07-03 09:00:47 +0200

    [Qt] call DumpBanlist() when baning unbaning nodes
    - this matches RPC call behaviour
    
    (cherry picked from commit 4ed05101f32c629008f7f962eb988e8078863fed)

--- src/qt/rpcconsole.cpp
+++ src/qt/rpcconsole.cpp
@@ -774,6 +774,7 @@ void RPCConsole::banSelectedNode(int bantime)
         SplitHostPort(nStr, port, addr);
 
         CNode::Ban(CNetAddr(addr), BanReasonManuallyAdded, bantime);
+        DumpBanlist(); // store banlist to disk
         bannedNode->fDisconnect = true;
 
         clearSelectedNode();
@@ -793,6 +794,7 @@ void RPCConsole::unbanSelectedNode()
     if (possibleSubnet.IsValid())
     {
         CNode::Unban(possibleSubnet);
+        DumpBanlist(); // store banlist to disk
         clientModel->getBanTableModel()->refresh();
     }
 }
