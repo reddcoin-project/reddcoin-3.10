874dc1f0c2d15d840f17e89fdc7e110fbca09a84
John Nash <redd@redd.ink>
2019-12-17 11:39:35 +1100

    remove hardcoded global variable rearrange debug log output

--- src/kernel.cpp
+++ src/kernel.cpp
@@ -563,12 +563,13 @@ double GetInflationAdjustment(const CBlockIndex* pindex)
 	double nRawInflationAdjustment = ((nInflationTarget / 12) * nRatio); // looking at the last month of blocks
 	double nInflationAdjustment = max(min(nRawInflationAdjustment, dMaxThreshold), dMinThreshold);
 
+	LogPrintf("- Inflation = %s.\n", (double(nPoSVRewards)/double(nMoneySupply)) * 12 * 100);
+	LogPrintf("- Inflation Bound Adjustment = %s. Using Max %s | Min %s thresholds\n", nInflationAdjustment, dMaxThreshold, dMinThreshold);
+
 	int64_t nTime = GetTimeMicros() - nStart;
-	fBenchmark = true;
+
 	if (fBenchmark)
-		LogPrintf("- Inflation = %s.\n", (double(nPoSVRewards)/double(nMoneySupply)) * 12 * 100);
 		LogPrintf("- Inflation Unbound Adjustment = %s Using last %s blocks from height %s to height %s: %.2fms\n", nRawInflationAdjustment, nBlocksPerMonth, nHeightPrev, pindex->nHeight, 0.001 * nTime);
-		LogPrintf("- Inflation Bound Adjustment = %s. Using Max %s | Min %s thresholds\n", nInflationAdjustment, dMaxThreshold, dMinThreshold);
 
 	return nInflationAdjustment;
 }
