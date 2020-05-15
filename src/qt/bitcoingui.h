652113632444d6b3bb57f9275d8e1780e04945b6
John Nash <redd@redd.ink>
2020-05-15 13:32:01 +1000

    revert definition for trayIconActivated on Mac OS

--- src/qt/bitcoingui.h
+++ src/qt/bitcoingui.h
@@ -200,10 +200,10 @@ private slots:
     void aboutClicked();
     /** Show help message dialog */
     void showHelpMessageClicked();
-// #ifndef Q_OS_MAC
+#ifndef Q_OS_MAC
     /** Handle tray icon clicked */
     void trayIconActivated(QSystemTrayIcon::ActivationReason reason);
-// #endif
+#endif
 
     /** Show window if hidden, unminimize when minimized, rise when obscured or show if hidden and fToggleHidden is true */
     void showNormalIfMinimized(bool fToggleHidden = false);
