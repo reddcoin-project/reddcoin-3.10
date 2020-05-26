tmagik/catoshi

Update generate-seeds.py to produce doxygen compatible comments

share/seeds/generate-seeds.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Copyright (c) 2014 Wladmir J. van der Laan
-# Distributed under the MIT/X11 software license, see the accompanying
+# Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 '''
 Script to generate list of seed nodes for chainparams.cpp.
@@ -116,10 +116,13 @@ def main():
     indir = sys.argv[1]
     g.write('#ifndef H_CHAINPARAMSSEEDS\n')
     g.write('#define H_CHAINPARAMSSEEDS\n')
-    g.write('// List of fixed seed nodes for the bitcoin network\n')
-    g.write('// AUTOGENERATED by contrib/devtools/generate-seeds.py\n\n')
-    g.write('// Each line contains a 16-byte IPv6 address and a port.\n')
-    g.write('// IPv4 as well as onion addresses are wrapped inside a IPv6 address accordingly.\n')
+    g.write('/**\n')
+    g.write(' * List of fixed seed nodes for the bitcoin network\n')
+    g.write(' * AUTOGENERATED by share/seeds/generate-seeds.py\n')
+    g.write(' *\n')
+    g.write(' * Each line contains a 16-byte IPv6 address and a port.\n')
+    g.write(' * IPv4 as well as onion addresses are wrapped inside a IPv6 address accordingly.\n')
+    g.write(' */\n')
     with open(os.path.join(indir,'nodes_main.txt'),'r') as f:
         process_nodes(g, f, 'pnSeed6_main', 8333)
     g.write('\n')

reddcoin-project/reddcoin-3.10

(cherry picked from commit 02fe12dcf4414eaff0161c6b5df7334135958485)
# Conflicts:
#	share/seeds/generate-seeds.py

share/seeds/generate-seeds.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 # Copyright (c) 2014 Wladmir J. van der Laan
-# Distributed under the MIT/X11 software license, see the accompanying
+# Distributed under the MIT software license, see the accompanying
 # file COPYING or http://www.opensource.org/licenses/mit-license.php.
 '''
 Script to generate list of seed nodes for chainparams.cpp.
@@ -116,10 +116,13 @@ def main():
     indir = sys.argv[1]
     g.write('#ifndef H_CHAINPARAMSSEEDS\n')
     g.write('#define H_CHAINPARAMSSEEDS\n')
-    g.write('// List of fixed seed nodes for the reddcoin network\n')
-    g.write('// AUTOGENERATED by contrib/devtools/generate-seeds.py\n\n')
-    g.write('// Each line contains a 16-byte IPv6 address and a port.\n')
-    g.write('// IPv4 as well as onion addresses are wrapped inside a IPv6 address accordingly.\n')
+    g.write('/**\n')
+    g.write(' * List of fixed seed nodes for the reddcoin network\n')
+    g.write(' * AUTOGENERATED by share/seeds/generate-seeds.py\n')
+    g.write(' *\n')
+    g.write(' * Each line contains a 16-byte IPv6 address and a port.\n')
+    g.write(' * IPv4 as well as onion addresses are wrapped inside a IPv6 address accordingly.\n')
+    g.write(' */\n')
     with open(os.path.join(indir,'nodes_main.txt'),'r') as f:
         process_nodes(g, f, 'pnSeed6_main', 45444)
     g.write('\n')