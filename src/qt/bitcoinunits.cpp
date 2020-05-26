tmagik/catoshi

[Qt] Replace status bar unit icon with actual images

src/qt/bitcoinunits.cpp
@@ -34,6 +34,17 @@ bool BitcoinUnits::valid(int unit)
     }
 }
 
+QString BitcoinUnits::id(int unit)
+{
+    switch(unit)
+    {
+    case BTC: return QString("btc");
+    case mBTC: return QString("mbtc");
+    case uBTC: return QString("ubtc");
+    default: return QString("???");
+    }
+}
+
 QString BitcoinUnits::name(int unit)
 {
     switch(unit)

reddcoin-project/reddcoin-3.10

(cherry picked from commit ad87bc4de10d8ff9934635f2a30078ccf531a124)

src/qt/bitcoinunits.cpp
@@ -34,6 +34,17 @@ bool BitcoinUnits::valid(int unit)
     }
 }
 
+QString BitcoinUnits::id(int unit)
+{
+    switch(unit)
+    {
+    case BTC: return QString("rdd");
+    case mBTC: return QString("mrdd");
+    case uBTC: return QString("urdd");
+    default: return QString("???");
+    }
+}
+
 QString BitcoinUnits::name(int unit)
 {
     switch(unit)