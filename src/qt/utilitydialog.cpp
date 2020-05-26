tmagik/catoshi

qt: Make splash and shutdown window ignore close events
It's strange to be able to close these windows while there is work
in progress.

Also set Qt::WA_DeleteOnClose on both windows to make sure that they
are deleted eventually, no matter what happens.

src/qt/utilitydialog.cpp
@@ -15,6 +15,7 @@
 
 #include <stdio.h>
 
+#include <QCloseEvent>
 #include <QLabel>
 #include <QRegExp>
 #include <QVBoxLayout>
@@ -106,22 +107,35 @@ void HelpMessageDialog::on_okButton_accepted()
 
 
 /** "Shutdown" window */
+ShutdownWindow::ShutdownWindow(QWidget *parent, Qt::WindowFlags f):
+    QWidget(parent, f)
+{
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addWidget(new QLabel(
+        tr("Bitcoin Core is shutting down...") + "<br /><br />" +
+        tr("Do not shut down the computer until this window disappears.")));
+    setLayout(layout);
+}
+
 void ShutdownWindow::showShutdownWindow(BitcoinGUI *window)
 {
     if (!window)
         return;
 
     // Show a simple window indicating shutdown status
-    QWidget *shutdownWindow = new QWidget();
-    QVBoxLayout *layout = new QVBoxLayout();
-    layout->addWidget(new QLabel(
-        tr("Bitcoin Core is shutting down...") + "<br /><br />" +
-        tr("Do not shut down the computer until this window disappears.")));
-    shutdownWindow->setLayout(layout);
+    QWidget *shutdownWindow = new ShutdownWindow();
+    // We don't hold a direct pointer to the shutdown window after creation, so use
+    // Qt::WA_DeleteOnClose to make sure that the window will be deleted eventually.
+    shutdownWindow->setAttribute(Qt::WA_DeleteOnClose);
     shutdownWindow->setWindowTitle(window->windowTitle());
 
     // Center shutdown window at where main window was
     const QPoint global = window->mapToGlobal(window->rect().center());
     shutdownWindow->move(global.x() - shutdownWindow->width() / 2, global.y() - shutdownWindow->height() / 2);
     shutdownWindow->show();
+}
+
+void ShutdownWindow::closeEvent(QCloseEvent *event)
+{
+    event->ignore();
 }

reddcoin-project/reddcoin-3.10

(cherry picked from commit cfc5cfb0f0d074774b17adcb88aaed11e6847c92)
# Conflicts:
#	src/qt/utilitydialog.cpp

src/qt/utilitydialog.cpp
@@ -15,6 +15,7 @@
 
 #include <stdio.h>
 
+#include <QCloseEvent>
 #include <QLabel>
 #include <QRegExp>
 #include <QVBoxLayout>
@@ -106,22 +107,35 @@ void HelpMessageDialog::on_okButton_accepted()
 
 
 /** "Shutdown" window */
+ShutdownWindow::ShutdownWindow(QWidget *parent, Qt::WindowFlags f):
+    QWidget(parent, f)
+{
+    QVBoxLayout *layout = new QVBoxLayout();
+    layout->addWidget(new QLabel(
+        tr("Reddcoin Core is shutting down...") + "<br /><br />" +
+        tr("Do not shut down the computer until this window disappears.")));
+    setLayout(layout);
+}
+
 void ShutdownWindow::showShutdownWindow(BitcoinGUI *window)
 {
     if (!window)
         return;
 
     // Show a simple window indicating shutdown status
-    QWidget *shutdownWindow = new QWidget();
-    QVBoxLayout *layout = new QVBoxLayout();
-    layout->addWidget(new QLabel(
-        tr("Reddcoin Core is shutting down...") + "<br /><br />" +
-        tr("Do not shut down the computer until this window disappears.")));
-    shutdownWindow->setLayout(layout);
+    QWidget *shutdownWindow = new ShutdownWindow();
+    // We don't hold a direct pointer to the shutdown window after creation, so use
+    // Qt::WA_DeleteOnClose to make sure that the window will be deleted eventually.
+    shutdownWindow->setAttribute(Qt::WA_DeleteOnClose);
     shutdownWindow->setWindowTitle(window->windowTitle());
 
     // Center shutdown window at where main window was
     const QPoint global = window->mapToGlobal(window->rect().center());
     shutdownWindow->move(global.x() - shutdownWindow->width() / 2, global.y() - shutdownWindow->height() / 2);
     shutdownWindow->show();
+}
+
+void ShutdownWindow::closeEvent(QCloseEvent *event)
+{
+    event->ignore();
 }