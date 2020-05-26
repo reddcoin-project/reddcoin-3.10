tmagik/catoshi

qt: Update some messages after suggestions by translators
- *cannot* is more common, thus preferred to *can not*
- Use *Watch-only* instead of *Watchonly* as one word

src/qt/paymentserver.cpp
@@ -424,7 +424,7 @@ void PaymentServer::handleURIOrFile(const QString& s)
             }
             else
                 emit message(tr("URI handling"),
-                    tr("URI can not be parsed! This can be caused by an invalid Bitcoin address or malformed URI parameters."),
+                    tr("URI cannot be parsed! This can be caused by an invalid Bitcoin address or malformed URI parameters."),
                     CClientUIInterface::ICON_WARNING);
 
             return;
@@ -438,7 +438,7 @@ void PaymentServer::handleURIOrFile(const QString& s)
         if (!readPaymentRequest(s, request))
         {
             emit message(tr("Payment request file handling"),
-                tr("Payment request file can not be read! This can be caused by an invalid payment request file."),
+                tr("Payment request file cannot be read! This can be caused by an invalid payment request file."),
                 CClientUIInterface::ICON_WARNING);
         }
         else if (processPaymentRequest(request, recipient))
@@ -663,7 +663,7 @@ void PaymentServer::netRequestFinished(QNetworkReply* reply)
         {
             qWarning() << "PaymentServer::netRequestFinished : Error parsing payment request";
             emit message(tr("Payment request error"),
-                tr("Payment request can not be parsed!"),
+                tr("Payment request cannot be parsed!"),
                 CClientUIInterface::MSG_ERROR);
         }
         else if (processPaymentRequest(request, recipient))

reddcoin-project/reddcoin-3.10

(cherry picked from commit bd26fee10bb2697340caf5e3f48922df0323f88e)
# Conflicts:
#	src/qt/intro.cpp
#	src/qt/locale/bitcoin_en.ts
#	src/qt/paymentserver.cpp

src/qt/paymentserver.cpp
@@ -424,7 +424,7 @@ void PaymentServer::handleURIOrFile(const QString& s)
             }
             else
                 emit message(tr("URI handling"),
-                    tr("URI can not be parsed! This can be caused by an invalid Reddcoin address or malformed URI parameters."),
+                    tr("URI cannot be parsed! This can be caused by an invalid Reddcoin address or malformed URI parameters."),
                     CClientUIInterface::ICON_WARNING);
 
             return;
@@ -438,7 +438,7 @@ void PaymentServer::handleURIOrFile(const QString& s)
         if (!readPaymentRequest(s, request))
         {
             emit message(tr("Payment request file handling"),
-                tr("Payment request file can not be read! This can be caused by an invalid payment request file."),
+                tr("Payment request file cannot be read! This can be caused by an invalid payment request file."),
                 CClientUIInterface::ICON_WARNING);
         }
         else if (processPaymentRequest(request, recipient))
@@ -663,7 +663,7 @@ void PaymentServer::netRequestFinished(QNetworkReply* reply)
         {
             qDebug() << "PaymentServer::netRequestFinished : Error parsing payment request";
             emit message(tr("Payment request error"),
-                tr("Payment request can not be parsed!"),
+                tr("Payment request cannot be parsed!"),
                 CClientUIInterface::MSG_ERROR);
         }
         else if (processPaymentRequest(request, recipient))