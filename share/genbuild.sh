5afd0f2394fb4f0bf085e7da4bee91975f46a7ac
Mylo Cyrus <irahiggins740@gmail.com>
2022-07-18 06:39:03 -0700

    }{

--- share/genbuild.sh
+++ share/genbuild.sh
@@ -33,7 +33,7 @@ if [ -e "$(which git 2>/dev/null)" -a "$(git rev-parse --is-inside-work-tree 2>/
     # get a string like "2012-04-10 16:27:19 +0200"
     LAST_COMMIT_DATE="$(git log -n 1 --format="%ci")"
 fi
-
+}{
 if [ -n "$DESC" ]; then
     NEWINFO="#define BUILD_DESC \"$DESC\""
 elif [ -n "$SUFFIX" ]; then
