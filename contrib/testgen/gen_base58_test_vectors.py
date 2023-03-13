aa0af63c7b2c7747765b51e4dc7c3182b608d447
John Nash <redd@redd.ink>
2023-03-13 13:02:01 +1100

    update base58 test vector generation for reddcoin

--- contrib/testgen/gen_base58_test_vectors.py
+++ contrib/testgen/gen_base58_test_vectors.py
@@ -15,11 +15,11 @@ import random
 from binascii import b2a_hex
 
 # key types
-PUBKEY_ADDRESS = 0
+PUBKEY_ADDRESS = 61
 SCRIPT_ADDRESS = 5
 PUBKEY_ADDRESS_TEST = 111
 SCRIPT_ADDRESS_TEST = 196
-PRIVKEY = 128
+PRIVKEY = 186
 PRIVKEY_TEST = 239
 
 metadata_keys = ['isPrivkey', 'isTestnet', 'addrType', 'isCompressed']
